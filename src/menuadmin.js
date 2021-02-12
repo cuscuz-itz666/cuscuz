ccca8ad5c3ae73a1d16e256914fa694dffc69a8f
Admcauss-alt <77009854+Admcauss-alt@users.noreply.github.com>
2021-02-12 07:43:00 -0300

    Update menuadmin.js

--- src/menuadmin.js
+++ src/menuadmin.js
@@ -2,29 +2,29 @@ const menuadmin = (prefix, pushname) => {
  return `oiin aqui e o menu dos admins
  _obs para o bot executar esses comandos e nescessario dar adm para o bot_
  
- ◪ *COMANDO DOS ADMINS*
+ 😳 *COMANDO DOS ADEME*
  │
- ├─ ❏ ${prefix}opengc
- ├─ ❏ ${prefix}closegc
- ├─ ❏ ${prefix}promote
- ├─ ❏ ${prefix}demote
- ├─ ❏ ${prefix}tagall
- ├─ ❏ ${prefix}tagall2
- ├─ ❏ ${prefix}tagall3
- ├─ ❏ ${prefix}tagall4
- ├─ ❏ ${prefix}tagall5
- ├─ ❏ ${prefix}add
- ├─ ❏ ${prefix}kick
- ├─ ❏ ${prefix}listadmins
- ├─ ❏ ${prefix}linkgroup
- ├─ ❏ ${prefix}leave
- ├─ ❏ ${prefix}welcome
- ├─ ❏ ${prefix}nsfw
- ├─ ❏ ${prefix}leveling
- ├─ ❏ ${prefix}level
- ├─ ❏ ${prefix}delete
- ├─ ❏ ${prefix}simih
- └─ ❏ ${prefix}ownergroup
+ ├─ 🐉 ${prefix}opengc
+ ├─ 🐉 ${prefix}closegc
+ ├─ 🐉 ${prefix}promote
+ ├─ 🐉 ${prefix}demote
+ ├─ 🐉 ${prefix}tagall
+ ├─ 🐉 ${prefix}tagall2
+ ├─ 🐉 ${prefix}tagall3
+ ├─ 🐉 ${prefix}tagall4
+ ├─ 🐉 ${prefix}tagall5
+ ├─ 🐉 ${prefix}add
+ ├─ 🐉 ${prefix}kick
+ ├─ 🐉 ${prefix}listadmins
+ ├─ 🐉 ${prefix}linkgroup
+ ├─ 🐉 ${prefix}leave
+ ├─ 🐉 ${prefix}welcome
+ ├─ 🐉 ${prefix}nsfw
+ ├─ 🐉 ${prefix}leveling
+ ├─ 🐉 ${prefix}level
+ ├─ 🐉 ${prefix}delete
+ ├─ 🐉 ${prefix}simih
+ └─ 🐉 ${prefix}ownergroup
  `
 
 
