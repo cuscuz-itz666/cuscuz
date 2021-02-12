30c674fc792b6d63a2c0a4514db4f21ef2e5983a
Admcauss-alt <77009854+Admcauss-alt@users.noreply.github.com>
2021-02-11 22:51:21 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -2,151 +2,151 @@ const help = (prefix) => {
 	return `
 「 *CAUSS BOT* 」
 
-◪ *informações*
-  ❏ Prefix: 「  ${prefix}  」
-  ❏ Criador : CAUSS (base toin) 
-  ❏ Como instalar o BOT: https://youtube.com/channel/UCpB3qh2Sp3K23s9a2Q-Gf-g
+🧐 *informações*
+  🐊 Prefix: 「  ${prefix}  」
+  🐊 Criador : CAUSS (base toin) 
+  🐊 Canal do criador: https://youtube.com/channel/UCpB3qh2Sp3K23s9a2Q-Gf-g
 
-◪ *SOBRE*
+🧐 *SOBRE*
   │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
-◪ *FAZER*
+  ├─ 🐊 ${prefix}info
+  ├─ 🐊 ${prefix}blocklist
+  ├─ 🐊 ${prefix}chatlist
+  ├─ 🐊 ${prefix}ping
+  └─ 🐊 ${prefix}bugreport
+🧐 *FAZER*
   │
-  ├─ ❏ ${prefix}sticker
-  ├─ ❏ ${prefix}stickergif
-  ├─ ❏ ${prefix}toimg
-  ├─ ❏ ${prefix}tomp3
-  ├─ ❏ ${prefix}bpink
-  ├─ ❏ ${prefix}marvellogo
-  ├─ ❏ ${prefix}snowwrite
-  ├─ ❏ ${prefix}3dtext
-  ├─ ❏ ${prefix}ninjalogo
-  ├─ ❏ ${prefix}water
-  ├─ ❏ ${prefix}firetext
-  ├─ ❏ ${prefix}logowolf
-  ├─ ❏ ${prefix}logowolf2
-  ├─ ❏ ${prefix}phlogo
-  ├─ ❏ ${prefix}glitch
-  ├─ ❏ ${prefix}neonlogo
-  ├─ ❏ ${prefix}neonlogo2
-  ├─ ❏ ${prefix}lionlogo
-  ├─ ❏ ${prefix}jokerlogo
-  ├─ ❏ ${prefix}shadow
-  ├─ ❏ ${prefix}burnpaper
-  ├─ ❏ ${prefix}coffee
-  ├─ ❏ ${prefix}lovepaper
-  ├─ ❏ ${prefix}woodblock
-  ├─ ❏ ${prefix}qowheart
-  ├─ ❏ ${prefix}mutgrass
-  ├─ ❏ ${prefix}undergocean
-  ├─ ❏ ${prefix}woodenboards
-  ├─ ❏ ${prefix}wolfmetal
-  ├─ ❏ ${prefix}metalictglow
-  ├─ ❏ ${prefix}8bit
-  ├─ ❏ ${prefix}ttp
-  ├─ ❏ ${prefix}herrypotter
-  ├─ ❏ ${prefix}pubglogo
-  └─ ❏ ${prefix}quotemaker
-◪ *MEDIA*
+  ├─ 🐊 ${prefix}sticker
+  ├─ 🐊 ${prefix}stickergif
+  ├─ 🐊 ${prefix}toimg
+  ├─ 🐊 ${prefix}tomp3
+  ├─ 🐊 ${prefix}bpink
+  ├─ 🐊 ${prefix}marvellogo
+  ├─ 🐊 ${prefix}snowwrite
+  ├─ 🐊 ${prefix}3dtext
+  ├─ 🐊 ${prefix}ninjalogo
+  ├─ 🐊 ${prefix}water
+  ├─ 🐊 ${prefix}firetext
+  ├─ 🐊 ${prefix}logowolf
+  ├─ 🐊 ${prefix}logowolf2
+  ├─ 🐊 ${prefix}phlogo
+  ├─ 🐊 ${prefix}glitch
+  ├─ 🐊 ${prefix}neonlogo
+  ├─ 🐊 ${prefix}neonlogo2
+  ├─ 🐊 ${prefix}lionlogo
+  ├─ 🐊 ${prefix}jokerlogo
+  ├─ 🐊 ${prefix}shadow
+  ├─ 🐊 ${prefix}burnpaper
+  ├─ 🐊 ${prefix}coffee
+  ├─ 🐊 ${prefix}lovepaper
+  ├─ 🐊 ${prefix}woodblock
+  ├─ 🐊 ${prefix}qowheart
+  ├─ 🐊 ${prefix}mutgrass
+  ├─ 🐊 ${prefix}undergocean
+  ├─ 🐊 ${prefix}woodenboards
+  ├─ 🐊 ${prefix}wolfmetal
+  ├─ 🐊 ${prefix}metalictglow
+  ├─ 🐊 ${prefix}8bit
+  ├─ 🐊 ${prefix}ttp
+  ├─ 🐊 ${prefix}herrypotter
+  ├─ 🐊 ${prefix}pubglogo
+  └─ 🐊 ${prefix}quotemaker
+◪🧐 *MEDIA*
   │
-  ├─ ❏ ${prefix}trendtwit
-  ├─ ❏ ${prefix}randomkpop
-  └─ ❏ ${prefix}ytsearch
-◪ *EDUCATION*
+  ├─ 🐊 ${prefix}trendtwit
+  ├─ 🐊 ${prefix}randomkpop
+  └─ 🐊 ${prefix}ytsearch
+🧐 *escolaKKKKK*
   │
-  ├─ ❏ ${prefix}wiki
-  ├─ ❏ ${prefix}wikien
-  ├─ ❏ ${prefix}nulis
-  ├─ ❏ ${prefix}quotes
-  ├─ ❏ ${prefix}quotes2
-  └─ ❏ ${prefix}artinama
-◪ *KERANG AJAIB*
+  ├─ 🐊 ${prefix}wiki
+  ├─ 🐊 ${prefix}wikien
+  ├─ 🐊 ${prefix}nulis
+  ├─ 🐊 ${prefix}quotes
+  ├─ 🐊 ${prefix}quotes2
+  └─ 🐊 ${prefix}artinama
+🧐 *KERANG AJAIB*
   │
-  ├─ ❏ ${prefix}apakah
-  ├─ ❏ ${prefix}kapankah
-  ├─ ❏ ${prefix}rate
-  └─ ❏ ${prefix}bisakah
-◪ *DOWNLOADER*
+  ├─ 🐊 ${prefix}apakah
+  ├─ 🐊 ${prefix}kapankah
+  ├─ 🐊 ${prefix}rate
+  └─ 🐊 ${prefix}bisakah
+🧐 *DOWNLOADER*
   │
-  ├─ ❏ ${prefix}images
-  ├─ ❏ ${prefix}ytmp3
-  ├─ ❏ ${prefix}ytmp4
-  ├─ ❏ ${prefix}tiktok
-  └─ ❏ ${prefix}joox
-◪ *MEME*
+  ├─ 🐊 ${prefix}images
+  ├─ 🐊 ${prefix}ytmp3
+  ├─ 🐊 ${prefix}ytmp4
+  ├─ 🐊 ${prefix}tiktok
+  └─ 🐊 ${prefix}joox
+🧐 *MEME*
   │
-  ├─ ❏ ${prefix}meme
-  └─ ❏ ${prefix}memeindo
-◪ *SOM*
+  ├─ 🐊 ${prefix}meme
+  └─ 🐊 ${prefix}memeindo
+🧐 *SOM*
   │
-  ├─ ❏ ${prefix}play
-  └─ ❏ ${prefix}tts
-◪ *MÚSICA*
+  ├─ 🐊 ${prefix}play
+  └─ 🐊 ${prefix}tts
+🧐 *MÚSICA*
   │
-  ├─ ❏ ${prefix}lirik
-  └─ ❏ ${prefix}chord
-◪ *ISLAM*
+  ├─ 🐊 ${prefix}lirik
+  └─ 🐊 ${prefix}chord
+🧐 *ISLAM*
   │
-  └─ ❏ ${prefix}quran
-◪ *STALK*
+  └─ 🐊 ${prefix}quran
+🧐 *STALK*
   │
-  ├─ ❏ ${prefix}tiktokstalk
-  └─ ❏ ${prefix}igstalk
-◪ *WIBU*
+  ├─ 🐊 ${prefix}tiktokstalk
+  └─ 🐊 ${prefix}igstalk
+🧐 *WIBU*
   │
-  ├─ ❏ ${prefix}neonime
-  ├─ ❏ ${prefix}pokemon
-  ├─ ❏ ${prefix}loli
-  ├─ ❏ ${prefix}waifu
-  ├─ ❏ ${prefix}randomanime
-  ├─ ❏ ${prefix}husbu
-  ├─ ❏ ${prefix}husbu2
-  ├─ ❏ ${prefix}wait
-  └─ ❏ ${prefix}nekonime
-◪ *DIVERSÃO*
+  ├─ 🐊 ${prefix}neonime
+  ├─ 🐊 ${prefix}pokemon
+  ├─ 🐊 ${prefix}loli
+  ├─ 🐊 ${prefix}waifu
+  ├─ 🐊 ${prefix}randomanime
+  ├─ 🐊 ${prefix}husbu
+  ├─ 🐊 ${prefix}husbu2
+  ├─ 🐊 ${prefix}wait
+  └─ 🐊 ${prefix}nekonime
+🧐 *DIVERSÃO*
   │
-  ├─ ❏ ${prefix}alay
-  ├─ ❏ ${prefix}gantengcek
-  ├─ ❏ ${prefix}watak
-  ├─ ❏ ${prefix}hobby
-  ├─ ❏ ${prefix}game
-  ├─ ❏ ${prefix}bucin
-  ├─ ❏ ${prefix}trust
-  ├─ ❏ ${prefix}dare
-  └─ ❏ ${prefix}simi
-◪ *INFORMAÇÃO*
+  ├─ 🐊 ${prefix}alay
+  ├─ 🐊 ${prefix}gantengcek
+  ├─ 🐊 ${prefix}watak
+  ├─ 🐊 ${prefix}hobby
+  ├─ 🐊 ${prefix}game
+  ├─ 🐊 ${prefix}bucin
+  ├─ 🐊 ${prefix}trust
+  ├─ 🐊 ${prefix}dare
+  └─ 🐊 ${prefix}simi
+🧐 *INFORMAÇÃO*
   │
-  ├─ ❏ ${prefix}bahasa
-  ├─ ❏ ${prefix}kodenegara
-  ├─ ❏ ${prefix}kbbi
-  ├─ ❏ ${prefix}fakta
-  ├─ ❏ ${prefix}infocuaca
-  ├─ ❏ ${prefix}infogempa
-  ├─ ❏ ${prefix}jadwaltvnow
-  └─ ❏ ${prefix}covid
-◪ *DONO*
+  ├─ 🐊 ${prefix}bahasa
+  ├─ 🐊 ${prefix}kodenegara
+  ├─ 🐊 ${prefix}kbbi
+  ├─ 🐊 ${prefix}fakta
+  ├─ 🐊 ${prefix}infocuaca
+  ├─ 🐊 ${prefix}infogempa
+  ├─ 🐊 ${prefix}jadwaltvnow
+  └─ 🐊 ${prefix}covid
+🧐 *COMANDOS DO CAUSS*
   │
-  ├─ ❏ ${prefix}setprefix
-  ├─ ❏ ${prefix}block
-  ├─ ❏ ${prefix}bc
-  ├─ ❏ ${prefix}bcgc
-  ├─ ❏ ${prefix}clone
-  └─ ❏ ${prefix}clearall
-◪ *OUTROS*
+  ├─ 🐊 ${prefix}setprefix
+  ├─ 🐊 ${prefix}block
+  ├─ 🐊 ${prefix}bc
+  ├─ 🐊 ${prefix}bcgc
+  ├─ 🐊 ${prefix}clone
+  └─ 🐊 ${prefix}clearall
+🧐 *Não faço ideia da categoria desses*
   │
-  ├─ ❏ ${prefix}send
-  ├─ ❏ ${prefix}wame
-  ├─ ❏ ${prefix}virtex
-  ├─ ❏ ${prefix}exe
-  ├─ ❏ ${prefix}qrcode
-  ├─ ❏ ${prefix}afk
-  ├─ ❏ ${prefix}timer
-  ├─ ❏ ${prefix}fml
-  └─ ❏ ${prefix}fml2
+  ├─ 🐊 ${prefix}send
+  ├─ 🐊 ${prefix}wame
+  ├─ 🐊 ${prefix}virtex
+  ├─ 🐊 ${prefix}exe
+  ├─ 🐊 ${prefix}qrcode
+  ├─ 🐊 ${prefix}afk
+  ├─ 🐊 ${prefix}timer
+  ├─ 🐊 ${prefix}fml
+  └─ 🐊 ${prefix}fml2
 `
 }
 
